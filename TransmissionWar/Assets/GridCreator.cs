9948b088779e4202cebfdd7e46ba8dfe3849b438
azb <azbaney@gmail.com>
2018-01-26 23:59:07 -0800

    Added Soldier Unit

--- TransmissionWar/Assets/GridCreator.cs
+++ TransmissionWar/Assets/GridCreator.cs
@@ -4,21 +4,61 @@ using UnityEngine;
 
 public class GridCreator : MonoBehaviour {
 
-    public Transform gridPrefab;
+    public Transform gridPrefab, kingPrefab, pigeonPrefab, soldierPrefab;
 
     public int gridWidth, gridHeight;
 
-	// Use this for initialization
-	void Start ()
+    public float tileWidth, tileHeight;
+
+    Transform mainCamera;
+
+    // Use this for initialization
+    void Start ()
     {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        mainCamera.position = new Vector3(gridWidth / 2 * tileWidth, Mathf.Max(gridWidth, gridHeight) * 10, gridWidth / 2 * tileWidth);
+
+        float xoffset = 0, yoffset = 0;
+
+        if (gridWidth % 2 == 0)
+        {
+            xoffset = 5;
+        }
+        if (gridHeight % 2 == 0)
+        {
+            yoffset = 5;
+        }
+
         for (int ix = 0; ix < gridWidth; ix++)
             for (int iy = 0; iy < gridHeight; iy++) {
-                Instantiate(gridPrefab, new Vector3(ix * 10 - 10 * gridWidth / 2, 0 , iy * 10 - 10 * gridHeight / 2), Quaternion.identity);
+                Instantiate(gridPrefab, new Vector3(ix * 10 + xoffset, 0 , iy * 10 + yoffset), Quaternion.identity);
             }
+
+        Instantiate(soldierPrefab, GridPosToWorldPos(1, 1), Quaternion.identity);
+
+        Instantiate(kingPrefab, GridPosToWorldPos(gridWidth/2, -1), Quaternion.identity);
+
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
+
+    Vector3 GridPosToWorldPos(int x, int y) {
+
+        float xoffset = 0, yoffset = 0;
+
+        if (gridWidth % 2 == 0)
+        {
+            xoffset = 5;
+        }
+        if (gridHeight % 2 == 0)
+        {
+            yoffset = 5;
+        }
+
+        return new Vector3(x * tileWidth + xoffset, 1, y * tileHeight + yoffset);
+    }
+    
 }
---
+++ TransmissionWar/Assets/UnitSelector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSelector : MonoBehaviour {
+
+    Camera cam;
+
+	// Use this for initialization
+	void Start () {
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+        RaycastHit hit;
+        Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit);
+        Vector3 point = hit.point;
+
+        Transform hitTransform = hit.transform;
+        if (hitTransform != null)
+            if (hitTransform.name.Contains("Soldier")) {
+                Debug.Log("hit");
+            }
+        }
+    }
+}
