7f2a2c332a2d2f7c0ff63320c6f75159adce32bd
paul <paul@cueb.io>
2018-01-27 15:53:57 -0800

    fix Direction.cs

--- TransmissionWar/Assets/Direction.cs
+++ TransmissionWar/Assets/Direction.cs
@@ -2,17 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< b38852c13dd6e119b39aa38b2151b30fffeb4bac
-public class Direction  {
-
-    //public enum Direction { UP, DOWN, LEFT, RIGHT };
-    public const int UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3;
-=======
 public class Direction {
 
     //public enum Direction { UP, DOWN, LEFT, RIGHT };
 
     public const int UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3, NONE = 4;
->>>>>>> checkin
 
 }
