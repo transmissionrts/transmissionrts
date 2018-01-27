2b2262e764a7a350aa9133f8f4aa70535a8575df
azb <azbaney@gmail.com>
2018-01-26 22:27:30 -0800

    Init

---
+++ TransmissionWar/Assets/GridCreator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCreator : MonoBehaviour {
+
+    public Transform gridPrefab;
+
+    public int gridWidth, gridHeight;
+
+	// Use this for initialization
+	void Start ()
+    {
+        for (int ix = 0; ix < gridWidth; ix++)
+            for (int iy = 0; iy < gridHeight; iy++) {
+                Instantiate(gridPrefab, new Vector3(ix * 10 - 10 * gridWidth / 2, 0 , iy * 10 - 10 * gridHeight / 2), Quaternion.identity);
+            }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
