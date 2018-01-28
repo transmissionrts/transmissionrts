77ee93cb2d3079f9c672760a52edb79abfb32a09
Daniel Geisler <dangeislerjr@gmail.com>
2018-01-27 18:39:53 -0800

    Added basic message passing

---
+++ TransmissionWar/Assets/GameNetwork/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TransmissionNetworking {
+	
+	public class Timer {
+		
+		private int startTimeMS;
+		private int delay;
+
+		public void set(int delay) {
+			this.delay = delay;
+		}
+
+		public void start() {
+			startTimeMS = (int)(Time.time * 1000f);
+		}
+
+		public bool expired() {
+			if(Time.time * 1000f > startTimeMS + delay) {
+				return true;
+			}
+			return false;
+		}
+	}
+
+}
