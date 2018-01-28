e4912b0b053b194f202b8047e1eabc8630b38d1e
azb <azbaney@gmail.com>
2018-01-27 18:08:33 -0800

    Removed grid update method

--- TransmissionWar/Assets/UnitSelector.cs
+++ TransmissionWar/Assets/UnitSelector.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitSelector : MonoBehaviour {
+
     Camera cam;
 
     public Transform selectedUnit, pigeon;
+
     public int nextCommand;
 
 	// Use this for initialization
@@ -15,7 +17,9 @@ public class UnitSelector : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonDown(0)) {
+
+        if (Input.GetMouseButtonDown(0))
+        {
         RaycastHit hit;
         Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit);
         Vector3 point = hit.point;
@@ -24,21 +28,14 @@ public class UnitSelector : MonoBehaviour {
         if (hitTransform != null)
             if (hitTransform.name.Contains("Soldier")) {
                 Debug.Log("hit");
-                SoldierController soldierController;
-                if (selectedUnit != null) {
-                    soldierController = selectedUnit.GetComponent<SoldierController>();
-                    soldierController.Deselect();
-                } 
+                    SelectableUnit su = hitTransform.GetComponent<SelectableUnit>();
+                    su.Select();
+                    selectedUnit = hitTransform;
 
-                selectedUnit = hitTransform;
-                soldierController = selectedUnit.GetComponent<SoldierController>();
-
-                if (soldierController.Team == GameManager.Instance.MyTeam) {
-                    soldierController.Select();
                     MoveableUnit unit = selectedUnit.GetComponent<MoveableUnit>();
                     unit.command = nextCommand;
+
                     pigeon.GetComponent<BirdMover>().SetTarget(selectedUnit);
-                }
             }
         }
     }
