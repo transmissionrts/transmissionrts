5cddc6a76ae766e8efc46239876f0557a1b303d0
azb <azbaney@gmail.com>
2018-01-28 00:20:39 -0800

    Merged

--- TransmissionWar/Assets/GridCreator.cs
+++ TransmissionWar/Assets/GridCreator.cs
@@ -4,25 +4,33 @@ using UnityEngine;
 
 
 [DefaultExecutionOrder(order: -1000)]
-public class GridCreator : MonoBehaviour {
+public class GridCreator : MonoBehaviour
+{
     public Transform gridPrefab, kingPrefab, pigeonPrefab, catSoldierPrefab, dogSoldierPrefab;
     public int gridWidth, gridHeight;
     public int soldierCount;
     public float tileWidth, tileHeight;
 
+    public Transform lightTile, darkTile;
+
     Transform mainCamera;
     public List<Transform> teamA;
-    Transform kingA;
-	public List<Transform> teamB;
-    Transform kingB;
+    Transform catKing;
+    public List<Transform> teamB;
+    Transform dogKing;
 
-    void SpawnSoldiers(List<Transform> container, int team, int startX, int startY) {
-		container.Clear ();
+    public Transform dogKingPrefab, catKingPrefab;
+
+    void SpawnSoldiers(List<Transform> container, int team, int startX, int startY)
+    {
+        container.Clear();
 
         Quaternion orient;
-        if (team == 0) {
+        if (team == 0)
+        {
             orient = Quaternion.Euler(0, 180, 0);
-        } else {
+        }
+        else {
             orient = Quaternion.Euler(0, 0, 0);
         }
 
@@ -30,10 +38,11 @@ public class GridCreator : MonoBehaviour {
 
         soldierPrefab[0] = catSoldierPrefab;
         soldierPrefab[1] = dogSoldierPrefab;
-        
-        for (int i = 0; i < soldierCount; i++) {
-            Transform soldier = Instantiate(soldierPrefab[team], position: GridPosToWorldPos(startX + i, startY), rotation: Quaternion.Euler(0,180,0)); //rotation: orient
-			soldier.name = string.Format("{0}_{1:00}", soldierPrefab[team].name, i); 
+
+        for (int i = 0; i < soldierCount; i++)
+        {
+            Transform soldier = Instantiate(soldierPrefab[team], position: GridPosToWorldPos(startX + i, startY), rotation: Quaternion.Euler(0, 180, 0)); //rotation: orient
+            soldier.name = string.Format("{0}_{1:00}", soldierPrefab[team].name, i);
             container.Add(soldier);
             SoldierController controller = soldier.GetComponent<SoldierController>();
         }
@@ -42,8 +51,9 @@ public class GridCreator : MonoBehaviour {
     }
 
     // Use this for initialization
-    void Awake ()
+    void Awake()
     {
+        soldierCount = gridWidth;
         mainCamera = GameObject.FindGameObjectWithTag("MainCamera").transform;
         mainCamera.position = new Vector3(gridWidth / 2 * tileWidth, Mathf.Max(gridWidth, gridHeight) * 10, gridWidth / 2 * tileWidth);
 
@@ -57,31 +67,38 @@ public class GridCreator : MonoBehaviour {
         {
             yoffset = 5;
         }
-
-        for (int ix = 0; ix < gridWidth; ix++) {
-            for (int iy = 0; iy < gridHeight; iy++) {
-				Transform tile = Instantiate(gridPrefab, new Vector3(ix * 10 + xoffset, 0 , iy * 10 + yoffset), Quaternion.identity);
-				tile.parent = this.transform;
+        
+        for (int ix = 0; ix < gridWidth; ix++)
+        {
+            for (int iy = 0; iy < gridHeight; iy++)
+            {
+                if ((ix+ iy) % 2 == 0)
+                    gridPrefab = lightTile;
+                else
+                    gridPrefab = darkTile;
+                Transform tile = Instantiate(gridPrefab, new Vector3(ix * 10 + xoffset, 0, iy * 10 + yoffset), Quaternion.identity);
+                tile.parent = this.transform;
             }
         }
 
-		LogicalGrid grid = this.gameObject.AddComponent<LogicalGrid> ();
-		grid.Setup (this.gridWidth, this.gridHeight);
+        LogicalGrid grid = this.gameObject.AddComponent<LogicalGrid>();
+        grid.Setup(this.gridWidth, this.gridHeight);
 
         // 0 for team A
-		this.teamA = new List<Transform>();
+        this.teamA = new List<Transform>();
         SpawnSoldiers(teamA, 0, 0, 0);
-        kingA = Instantiate(kingPrefab, GridPosToWorldPos(gridWidth/2, -1), Quaternion.identity);
-		grid.KingATransform = kingA;
+        catKing = Instantiate(catKingPrefab, GridPosToWorldPos(-1, 0), Quaternion.Euler(0, 180, 0));
+        grid.KingATransform = catKing;
 
         // 1 for team B
-		this.teamB = new List<Transform>();
-        SpawnSoldiers(teamB, 1, 0, gridHeight -1 ); //Mathf.RoundToInt(gridWidth),
-        kingB = Instantiate(kingPrefab, GridPosToWorldPos(gridWidth/2, gridHeight), Quaternion.identity);
-		grid.KingBTransform = kingB;
+        this.teamB = new List<Transform>();
+        SpawnSoldiers(teamB, 1, 0, gridHeight - 1); //Mathf.RoundToInt(gridWidth),
+        dogKing = Instantiate(dogKingPrefab, GridPosToWorldPos(-1, gridHeight-1), Quaternion.Euler(0,180,0));
+        grid.KingBTransform = dogKing;
     }
-    
-    public Vector3 GridPosToWorldPos(int x, int y) {
+
+    public Vector3 GridPosToWorldPos(int x, int y)
+    {
 
         float xoffset = 0, yoffset = 0;
 
@@ -97,7 +114,8 @@ public class GridCreator : MonoBehaviour {
         return new Vector3(x * tileWidth + xoffset, 1, y * tileHeight + yoffset);
     }
 
-	public LogicalGrid GetGrid(){
-		return this.GetComponent<LogicalGrid> ();
-	}    
+    public LogicalGrid GetGrid()
+    {
+        return this.GetComponent<LogicalGrid>();
+    }
 }
