997f83a35297ccb4491edacc259d7e49e780dc54
Daniel Geisler <dangeislerjr@gmail.com>
2018-01-28 11:16:35 -0800

    Added Networked opponent scene, direction to networked direction helper

---
+++ TransmissionWar/Assets/GameNetwork/NetworkedDirectionHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkedDirectionHelper {
+
+	static Direction getDirection(Direction d) {
+		switch(d) {
+		case Direction.UP:
+			return Direction.DOWN;
+
+		case Direction.DOWN:
+			return Direction.UP;
+
+		case Direction.LEFT:
+			return Direction.RIGHT;
+
+
+		case Direction.RIGHT:
+			return Direction.LEFT;
+		}
+	}
+}
---
+++ TransmissionWar/Assets/GameNetwork/NetworkedPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkedPlayer : AbstractPlayer {
+
+	// Use this for initialization
+	void Start () {
+
+		base.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void PlayTurn(){
+		base.PlayTurn ();
+//		List<SoldierController> myUnits = this.GetMyUnits ();
+//		this.aiData.myScoredUnits = this.ScoreUnits(myUnits, 0);
+//
+//		this.rootNode.Tick (new TimeData (Time.deltaTime));
+//
+//		this.gameManager.EndTurn (this.playerId);
+
+//		int selectedMoveIdx = Random.Range (0, posibleMoves.Count);
+//		Debug.LogFormat(this, "{0}.selectedMove: {1}", this.name,  posibleMoves [selectedMoveIdx]);
+//		this.gameManager.IssueCommandTo (this.playerId, scored.Soldier, posibleMoves [selectedMoveIdx]);
+	}
+
+	public override void TurnEnded ()
+	{
+		base.TurnEnded ();
+	}
+
+	public override void ResetTurn ()
+	{
+		base.ResetTurn ();
+	}
+}
